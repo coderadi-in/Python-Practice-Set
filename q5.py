e5d1cfe1ed12fd65622b18e1e343edf9dddcdcb0
Aditya <adiprofitcoder@gmail.com>
2025-07-10 12:46:01 +0530

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,20 @@
+'''
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+'''
+
+class JustAClass():
+  	def getString(self):
+    	return input("Give a console input: ")
+
+  	def printString(self, string: str):
+   		print(string)
+
+  	def test(self):
+	    data = self.getString()
+	    self.printString(data)
+
+cls = JustAClass()
+cls.test()
